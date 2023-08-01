cdd4680a376e7a2c9bc8601de1ce6a5f039f1eaa
funkadaft <134439863+funkadaft@users.noreply.github.com>
2023-08-01 15:27:49 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,46 +1,11 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "663bff60",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "from setuptools import setup\n",
-    "\n",
-    "\n",
-    "setup(\n",
-    "    name='FARGO3DVisualize',\n",
-    "    version='1.0.0',\n",
-    "\n",
-    "    url='https://github.com/RaffoSchuda/FARGO3DVisualize.git',\n",
-    "    author='Nicolás Campos, Antonia Morales, Martín Raffo & Vicente Silva.'\n",
-    "\n",
-    "    install_requires=['numpy','matplotlib'],\n",
-    ")"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3 (ipykernel)",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.8.10"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+from setuptools import setup\n",
+
+    setup(
+        name='FARGO3DVisualize',
+        version='1.0.0',
+    
+        url='https://github.com/RaffoSchuda/FARGO3DVisualize.git',
+        author='Nicolás Campos, Antonia Morales, Martín Raffo & Vicente Silva.',
+        install_requires=['numpy','matplotlib'],
+    )
+  
