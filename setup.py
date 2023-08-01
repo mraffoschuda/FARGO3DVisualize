291866125c6231eb8f9d474fbea5cdebf6f68e15
funkadaft <134439863+funkadaft@users.noreply.github.com>
2023-08-01 15:28:53 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
-from setuptools import setup\n",
+from setuptools import setup
 
-    setup(
-        name='FARGO3DVisualize',
-        version='1.0.0',
+setup(
+    name='FARGO3DVisualize',
+    version='1.0.0',
     
-        url='https://github.com/RaffoSchuda/FARGO3DVisualize.git',
-        author='Nicolás Campos, Antonia Morales, Martín Raffo & Vicente Silva.',
-        install_requires=['numpy','matplotlib'],
-    )
+    url='https://github.com/RaffoSchuda/FARGO3DVisualize.git',
+    author='Nicolás Campos, Antonia Morales, Martín Raffo & Vicente Silva.',
+    install_requires=['numpy','matplotlib'])
   
